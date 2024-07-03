cd70e40275bb4f1602c1a1bbf1a3d6c7263b3253
Laurine Ndanu Nzuki <nzukilaurine@gmail.com>
2024-07-03 13:04:36 +0300

    All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+saturdayFun();
+saturdayFun("bathe my dog");
+
+//
+const mondayWork = function(actMonday = "go to the office"){
+    return (`This Monday, I will ${actMonday}.`);
+}
+mondayWork();
+mondayWork("go to the nail salon");
+
+//
+function wrapAdjective(description = '*'){
+    const selfAffirmations = function(adj = "special"){
+        return `You are ${description}${adj}${description}!`;
+    };
+    return selfAffirmations;
+}
+wrapAdjective();
+wrapAdjective('*');
+wrapAdjective('||');
